70e5fe5351436de439036c2b8990bb52ee0b8a17
Sergiusz Kriwobokow <writesd@hotmail.com>
2022-11-14 14:12:42 +0100

    no comments

---
+++ src/BlazorClientStart.Domain.Shared/Option/EmptyOptionNameException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorClientStart.Domain.Shared.Option
+{
+    internal class EmptyOptionNameException : Exception
+    {
+        public EmptyOptionNameException(string message)
+            : base(message) { }
+    }
+}
---
+++ src/BlazorClientStart.Domain.Shared/Option/OptionBase.cs
@@ -0,0 +1,23 @@
+﻿namespace BlazorClientStart.Domain.Shared.Option
+{
+    public abstract class OptionBase<TValue>
+    {
+        protected OptionBase() { }
+
+        protected internal OptionBase(string name, TValue value)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new EmptyOptionNameException(nameof(Name));
+
+            Id = Guid.NewGuid();
+            Name = name;
+            Value = value;
+        }
+
+        public Guid Id { get; }
+
+        public string Name { get; private set; }
+
+        public TValue Value { get; set; }
+    }
+}
