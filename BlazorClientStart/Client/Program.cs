2562033ca9480b8a53486ac82a029afed2cd3db9
Sergiusz Kriwobokow <writesd@hotmail.com>
2022-11-14 02:52:20 +0100

    no comments

--- BlazorClientStart/Client/Program.cs
+++ BlazorClientStart/Client/Program.cs
@@ -1,6 +1,7 @@
 using BlazorClientStart;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
 
 namespace BlazorClientStart
 {
@@ -8,11 +9,17 @@ namespace BlazorClientStart
     {
         public static async Task Main(string[] args)
         {
+            // Syncfusion components version: 20.3.0.52
+            const string LICENSE_KEY = "NzU1NzY3QDMyMzAyZTMzMmUzMGhTL1Z4aTVGcFJjTkJrR2xuaGVIcjdBSXcyTDVBK1RFdzNPRURqQTJZc2M9";
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(LICENSE_KEY);
+
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddSyncfusionBlazor();
 
             await builder.Build().RunAsync();
         }
