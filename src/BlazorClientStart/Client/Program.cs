5b959faf17317de7e02c93b1fe0d2bc51da3374b
Sergiusz Kriwobokow <writesd@hotmail.com>
2022-11-25 13:17:37 +0100

    mo comments

--- src/BlazorClientStart/Client/Program.cs
+++ src/BlazorClientStart/Client/Program.cs
@@ -9,8 +9,8 @@ namespace BlazorClientStart
     {
         public static async Task Main(string[] args)
         {
-            // Syncfusion components version: 20.3.0.52
-            const string LICENSE_KEY = "NzU1NzY3QDMyMzAyZTMzMmUzMGhTL1Z4aTVGcFJjTkJrR2xuaGVIcjdBSXcyTDVBK1RFdzNPRURqQTJZc2M9";
+            // Syncfusion components version: 20.3.0.56
+            const string LICENSE_KEY = "NzY4MjQ4QDMyMzAyZTMzMmUzMExNbkN6czhDOGhqY2hCSXhyMWpYTnIxbjdhZlgzVWlRN2pGQy9hMzc4WUE9";
             Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(LICENSE_KEY);
 
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
