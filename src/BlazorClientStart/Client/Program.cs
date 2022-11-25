e5ef95286ea6a03fe9d87578eacde5fc7ecfac31
Sergiusz Kriwobokow <writesd@hotmail.com>
2022-11-25 13:07:47 +0100

    added tag <app>

--- src/BlazorClientStart/Client/Program.cs
+++ src/BlazorClientStart/Client/Program.cs
@@ -14,14 +14,17 @@ namespace BlazorClientStart
             Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(LICENSE_KEY);
 
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<App>("app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             builder.Services.AddSyncfusionBlazor();
 
-            await builder.Build().RunAsync();
+
+        
+
+        await builder.Build().RunAsync();
         }
     }
 }
